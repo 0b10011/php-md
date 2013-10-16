2c499103eaf1095e315b265951358cc4ce8c6da2
Brandon Frohs <brandon@forewordreviews.com>
2013-10-16 15:15:34 -0400

    Simplify call to Markdown() in tests.

--- tests/app.test.php
+++ tests/app.test.php
@@ -1,5 +1,7 @@
 <?php
 
+use bfrohs\markdown\Markdown as Markdown;
+
 // Include array functions
 require_once(MARKDOWN_SOURCE.'/app.php');
 
@@ -7,31 +9,31 @@ class MarkdownTest extends PHPUnit_Framework_TestCase {
 	
 	public function testParagraphs(){
 		$text = "foo\nbar\n\nhello\n\n\nworld";
-		$markdown = new bfrohs\markdown\Markdown($text);
+		$markdown = new Markdown($text);
 		$this->assertEquals($markdown->toHTML(), '<p>foo bar</p><p>hello</p><p>world</p>');
 	}
 	
 	public function testNewLines(){
 		$text = "foo\n bar \n hello  \n world";
-		$markdown = new bfrohs\markdown\Markdown($text);
+		$markdown = new Markdown($text);
 		$this->assertEquals($markdown->toHTML(), '<p>foo bar hello<br>world</p>');
 	}
 	
 	public function testEm(){
 		$text = "foo *bar*";
-		$markdown = new bfrohs\markdown\Markdown($text);
+		$markdown = new Markdown($text);
 		$this->assertEquals($markdown->toHTML(), '<p>foo <em>bar</em></p>');
 	}
 	
 	public function testStrong(){
 		$text = "foo **bar**";
-		$markdown = new bfrohs\markdown\Markdown($text);
+		$markdown = new Markdown($text);
 		$this->assertEquals($markdown->toHTML(), '<p>foo <strong>bar</strong></p>');
 	}
 	
 	public function testEmStrong(){
 		$text = "foo ***bar***";
-		$markdown = new bfrohs\markdown\Markdown($text);
+		$markdown = new Markdown($text);
 		$this->assertEquals($markdown->toHTML(), '<p>foo <strong><em>bar</em></strong></p>');
 	}
 }
